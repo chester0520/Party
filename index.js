a19dcf55d133956c4f010600253067bc28a872b5
chester0520 <clemo0520@gmail.com>
2021-05-10 15:18:24 +0800

    完成串接 indievox 和 拓元

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ bot.on('message', async event => {
   if (event.message.type === 'text') {
     try {
       const response = await axios.get(`https://www.indievox.com/activity/list/${encodeURI(event.message.text)}`)
+      const response1 = await axios.get(`https://tixcraft.com/activity/list/search/${encodeURI(event.message.text)}`)
+      // const response2 = await axios.get(`https://kktix.com/events?utf8=%E2%9C%93&search=${encodeURI(event.message.text)}`)
       const $ = cheerio.load(response.data)
+      const $$ = cheerio.load(response1.data)
+      // const $$$ = cheerio.load(response2.data)
       const flexarr = []
       let thistext = ''
-      $('.panel-body').each(function (r) {
-        // for (i = 0; i <= r; i++) {
-        //   console.log(i)
-        // }
+      $('.panel-body').each(function () {
         const flex = {
           type: 'bubble',
           hero: {
@@ -108,10 +109,95 @@ bot.on('message', async event => {
             flex: 0
           }
         }
-        flexarr.push(flex)
         thistext += $(this).text()
-        // const response1 = await axios.get(`$(this).find('a').attr('href')`)
+        flexarr.push(flex)
       })
+      $$('#selling .thumbnail').each(function () {
+        const flex1 = {
+          type: 'bubble',
+          hero: {
+            type: 'image',
+            url: $(this).find('img').attr('src'),
+            size: 'full',
+            aspectRatio: '850:370',
+            aspectMode: 'fit',
+            action: {
+              type: 'uri',
+              uri: 'https://tixcraft.com' + $$(this).find('a').attr('href')
+            }
+          },
+          body: {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'text',
+                text: $$(this).find('a').text(),
+                weight: 'bold',
+                size: 'md',
+                wrap: true
+              },
+              {
+                type: 'box',
+                layout: 'vertical',
+                margin: 'lg',
+                spacing: 'sm',
+                contents: [
+                  {
+                    type: 'box',
+                    layout: 'baseline',
+                    spacing: 'sm',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: '時間',
+                        color: '#aaaaaa',
+                        size: 'sm',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: $$(this).find('span').text(),
+                        wrap: true,
+                        color: '#666666',
+                        size: 'sm',
+                        flex: 5
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          footer: {
+            type: 'box',
+            layout: 'vertical',
+            spacing: 'sm',
+            contents: [
+              {
+                type: 'button',
+                style: 'link',
+                height: 'sm',
+                action: {
+                  type: 'uri',
+                  label: '前往購票網站',
+                  uri: 'https://tixcraft.com' + $$(this).find('a').attr('href')
+                }
+              },
+              {
+                type: 'spacer',
+                size: 'sm'
+              }
+            ],
+            flex: 0
+          }
+        }
+        thistext += $$(this).find('a').text()
+        flexarr.push(flex1)
+      })
+      // $$$('figure').each(function () {
+      //   console.log(this)
+      // })
       const message = {
         type: 'flex',
         altText: thistext,
